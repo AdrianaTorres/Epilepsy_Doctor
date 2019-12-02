057276e819abaabfe0f76baa3f648d9fd4d92a11
Lucia <Lucia@10.60.58.38>
2019-12-02 14:16:15 +0100

    whatev

--- src/guiDoctor/StaticGraph.java
+++ src/guiDoctor/StaticGraph.java
@@ -3,11 +3,13 @@ package guiDoctor;
 import java.awt.BorderLayout;
 import java.util.List;
 import javax.swing.JPanel;
+
 import org.knowm.xchart.XChartPanel;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 
-public class StaticGraph {
+
+public class StaticGraph extends JPanel{
 	public StaticGraph(List <Double>[] data,String plotType) {
 
 		XYChart chart = new XYChartBuilder().width(400).height(200).title(plotType).build();
